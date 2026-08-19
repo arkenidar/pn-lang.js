e44d4861e935737a42766d3d2a19aaf9c4b530ee
Dario Cangialosi @Arkenidar <dario.cangialosi@gmail.com>
2026-08-19 15:11:21 +0200

    Fix escaping logic and update test program string in parse.js

--- core/parse.js
+++ core/parse.js
@@ -1,26 +1,37 @@
 
 var write_line = console.log
-var program_test_1 = ' @write-line "hello dear friends " '
+var program_test_1 = ' @write-line "hello dear friends \\" \\n \\ \\\\ \\ \\" " '
 
 var mode=''
 var token=''
 var list=[]
+var escaping=false
 var program = program_test_1
 for(var i=0; i<program.length; i++){
-	var character=program[i]
+	var character = program[i]
+	var next_character = ''
+	if((i+1)<program.length)
+		next_character=program[i+1]
 	//write_line(character)
 
 	if(mode=='')
 		if(character=='@' || character=='"')
 			{ mode=character ; continue }
+	
+	if(!escaping && character=='\\' && ['\\','"','n'].indexOf(next_character) != -1 )
+		{ escaping=true ; continue }
 
-	var stop=mode=='"' && character=='"'
+	var stop=mode=='"' && character=='"' && !escaping
 	stop ||= mode=='@' && character==' '
 	if(stop) {
 		list.push([mode,token])
 		mode='' ; token=''
 	}
 
+	if(escaping && character=='\\') character='\\'
+	if(escaping && character=='"') character='"'
+	if(escaping && character=='n') character='\n'
 	if(mode!='') token += character
+	escaping=false
 }
 write_line(list)
